597c393369f4483a9634c2058756c176889bcc48
Matheus <matheus.goulart@penseavanti.com.br>
2022-12-23 20:46:51 -0300

    chore: .gitignore update

---
+++ imagetoolkit/views/html_to_image.py
@@ -0,0 +1,20 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authentication import authenticate
+from django.http import HttpResponse
+
+from html2image import Html2Image
+hti = Html2Image()
+
+@api_view(['GET', 'POST'])
+def html_to_image(request):
+    body = request.data
+    
+    hti.screenshot(html_str=body["html"],
+                   css_str=body["css"], save_as=body["filename"], size=(body["width"], body["height"]))
+    
+    file = open(body["filename"], 'rb')
+    
+    response = HttpResponse(file, content_type='image/png')
+    response['Content-Disposition'] = 'attachment; filename=NameOfFile'
+    return response
