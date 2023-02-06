e9f263747ee63a05f08ff554e37eae2a9a37ee04
mathealgou <mathealgou@gmail.com>
2023-02-05 22:22:02 -0300

    feat: Added some input validation t the "process" route.

--- imagetoolkit/views/process/process.py
+++ imagetoolkit/views/process/process.py
@@ -1,9 +1,23 @@
 from .helpers import crop_image, saturate_image, vignette
+from django.http import JsonResponse
+
+# Used for input validation later on
+ACTIONS = [
+    "crop",
+    "saturate",
+    "vignette",
+]
+
 
 def process_image(image, processess):
     unprocessed_image = image
-    
+
     for process in processess:
+        if process["action"] not in ACTIONS:
+            return JsonResponse(
+                {"error": "Invalid action: {}".format(process["action"])}
+            )
+
         if process["action"] == "crop":
             image = crop_image(image, process)
         elif process["action"] == "saturate":
